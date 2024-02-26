2ea809d3d887ff04455f61df3a0b70dad7639849
Alexander Barmin <barmin.am@gmail.com>
2024-02-26 18:51:29 +0000

    feat(js): add class MyItem

--- js-hw-list/list.js
+++ js-hw-list/list.js
@@ -3,3 +3,29 @@
     Написать класс, реализующий двусвязный список.
     Предусмотреть методы поиска, вставки, удаления, изменения элемента и определения длины списка.
 */
+
+class MyItem {
+    constructor(_value) {
+        this._value = _value;
+        this._next = null;
+        this._prev = null;
+    }
+    get value() {
+        return this._value;
+    }
+    set value(value) {
+        this._value = value;
+    }
+    set next(node) {
+        this._next = node;
+    }
+    set prev(node) {
+        this._prev = node;
+    }
+    get next() {
+        return this._next;
+    }
+    get prev() {
+        return this._prev;
+    }
+}
