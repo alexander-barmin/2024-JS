0d4dbe4ca23cc471f1ab7f420cc3523fc5ddfeaa
Alexander Barmin <barmin.am@gmail.com>
2024-02-26 19:47:37 +0000

    feat(js): add class MyList

--- js-hw-list/list.js
+++ js-hw-list/list.js
@@ -5,8 +5,8 @@
 */
 
 class MyItem {
-    constructor(_value) {
-        this._value = _value;
+    constructor(value) {
+        this._value = value;
         this._next = null;
         this._prev = null;
     }
@@ -29,3 +29,62 @@ class MyItem {
         return this._prev;
     }
 }
+
+class MyList {
+    constructor() {
+        this._start = null;
+        this._end = null;
+        this._count = 0;
+    }
+
+    set start(item) {
+        this._start = item;
+    }
+    set end(item) {
+        this._end = item;
+    }
+    get start() {
+        return this._start;
+    }
+    get end() {
+        return this._end;
+    }
+    get count() {
+        return this._count;
+    }
+
+    append(value) {
+        const item = new MyItem(value);
+        if (!this._start) {
+            this._start = item;
+        }
+        if (this._end) {
+            this._end.next = item;
+        }
+        item.prev = this._end;
+        this._end = item;
+        this._count++;
+        return this;
+    }
+    print() {
+        if (!this._start) {
+            console.log('empty!');
+        }
+        else {
+            let curr = this._start;
+            while(curr) {
+                console.log(curr && curr.value);
+                curr = curr.next;
+            }
+        }
+    }
+}
+
+const item = new MyItem('str1');
+console.log('my first item:', item && item.value);
+
+const list = new MyList();
+list.append("str1");
+list.append("str2");
+console.log('my first list (count item %d):', list && list.count);
+list.print();
