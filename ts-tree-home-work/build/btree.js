a8b7fed9ca38284eb6e9badae0cec4370ef00ba4
Alexander Barmin <barmin.am@gmail.com>
2024-03-03 17:56:19 +0000

    refactor the code and add a height delete method

--- ts-tree-home-work/build/btree.js
+++ ts-tree-home-work/build/btree.js
@@ -19,6 +19,11 @@ class MyNode {
     set right(val) {
         this._right = val;
     }
+    get height() {
+        let leftHeight = this.left ? this.left.height + 1 : 0;
+        let rightHeight = this.right ? this.right.height + 1 : 0;
+        return Math.max(leftHeight, rightHeight);
+    }
     get value() {
         return this._value;
     }
@@ -33,34 +38,32 @@ class MyBinaryTree {
     constructor() {
         this._root = null;
     }
-    add(value) {
-        const newNode = new MyNode(value);
-        if (!this._root) {
-            this._root = newNode;
-            return newNode;
+    addItem(newItem) {
+        const newNode = new MyNode(newItem);
+        if (this._root === null) {
+            return this._root = newNode;
         }
-        let curNode = this._root;
-        while (curNode) {
-            if (newNode.value < curNode.value) {
-                if (!curNode.left) {
-                    curNode.left = newNode;
-                    return newNode;
-                }
-                else {
-                    curNode = curNode.left;
-                }
+        return this._addNode(this._root, newNode);
+    }
+    _addNode(rootNode, newNode) {
+        let retVal;
+        if (newNode.value < rootNode.value) {
+            if (rootNode.left === null) {
+                retVal = rootNode.left = newNode;
             }
             else {
-                if (!curNode.right) {
-                    curNode.right = newNode;
-                    return newNode;
-                }
-                else {
-                    curNode = curNode.right;
-                }
+                retVal = this._addNode(rootNode.left, newNode);
             }
         }
-        return newNode;
+        else {
+            if (rootNode.right === null) {
+                retVal = rootNode.right = newNode;
+            }
+            else {
+                retVal = this._addNode(rootNode.right, newNode);
+            }
+        }
+        return retVal;
     }
     preOrder(node, callback) {
         if (!node) {
@@ -101,54 +104,121 @@ class MyBinaryTree {
         }
         return this.postOrder(this._root, callback);
     }
-    recursionSearch(node, value) {
-        if (!node) {
-            return;
+    searchItem(itemValue) {
+        return this._searchNode(this._root, itemValue);
+    }
+    _searchNode(node, itemValue) {
+        if (node === null) {
+            return null;
         }
-        if (node.value === value) {
-            console.log('!!!', node.value);
+        if (node.value === itemValue) {
+            return node;
         }
-        console.log(node.value);
-        if (node.value > value) {
-            this.recursionSearch(node.left, value);
+        if (itemValue < node.value) {
+            return this._searchNode(node.left, itemValue);
         }
-        if (node.value < value) {
-            this.recursionSearch(node.right, value);
+        else {
+            return this._searchNode(node.right, itemValue);
         }
     }
-    searchNode(value) {
-        this.recursionSearch(this._root, value);
+    changeNode(value, newValue) {
+        const found = this.searchItem(value);
+        if (found) {
+            found.value = newValue;
+        }
+        return found;
+    }
+    deleteItem(itemValue) {
+        if (this._root === null) {
+            return null;
+        }
+        return this._root = this._deleteNode(this._root, itemValue);
+    }
+    _deleteNode(currentNode, itemValue) {
+        // если нашли нужный элемент, начинаем процедуру удаления
+        if (currentNode.value === itemValue) {
+            // обработка самого простого случая, вместо узла возвращается null
+            if (currentNode.left === null && currentNode.right === null) {
+                return null;
+            }
+            // обработка двух случаев, с только одним из поддеревьев
+            if (currentNode.left === null) {
+                return currentNode.right;
+            }
+            if (currentNode.right === null) {
+                return currentNode.left;
+            }
+            // если у ноды есть оба потомка
+            const minNodeInRightSubtree = this._findMinElementRight(currentNode.right);
+            // заменили текущий элемент минимальным из правого поддерева
+            currentNode.value = minNodeInRightSubtree.value;
+            // ищем в правом поддереве минимальный элемент,
+            // значение которого уже вставлено на место текущего
+            currentNode.right = this._deleteNode(currentNode.right, minNodeInRightSubtree.value);
+            return currentNode;
+        }
+        // попадаем сюда, если элемент не был найден,
+        // просто проваливаемся в дерево глубже и глубже
+        // производится рекурсивный вызов этой же функции,
+        // при этом если элемент не будет найден,
+        // то алгоритм просто будет возвращать существующую ссылку на поддерево,
+        // которая присвоится в ту же позицию
+        if (itemValue < currentNode.value) {
+            if (currentNode.left === null) {
+                console.warn('Item not found!');
+                return currentNode;
+            }
+            // проваливаемся в левое поддерево,
+            // после рекурсивной отработки функции _deleteNode
+            // будет возвращен текущий элемент,
+            // который в предыдущем вызове будет присвоен
+            currentNode.left = this._deleteNode(currentNode.left, itemValue);
+            // присваивание на рекурсивный уровень выше,
+            // может быть как в левое поддерево,так и в правое,
+            // на текущем уровне мы не знаем какое поддерево обрабатываем
+            return currentNode;
+        }
+        // аналогичная обработка для правого поддерева
+        if (itemValue > currentNode.value) {
+            if (currentNode.right === null) {
+                console.warn('Item not found!');
+                return currentNode;
+            }
+            currentNode.right = this._deleteNode(currentNode.right, itemValue);
+            return currentNode;
+        }
+    }
+    _findMinElementRight(node) {
+        if (node.left === null) {
+            return node;
+        }
+        return this._findMinElementRight(node.left);
+    }
+    get hight() {
+        if (this._root === null) {
+            return 0;
+        }
+        return this._root.height;
     }
 }
 const myTree = new MyBinaryTree();
-myTree.add(5);
-myTree.add(6);
-myTree.add(4);
-myTree.add(8);
-myTree.add(7);
-myTree.add(3);
-myTree.add(2);
-myTree.add(4);
-myTree.add(1);
-//console.log(myTree);
-console.log('--DFS pre--');
+//--- insert item
+myTree.addItem(5);
+myTree.addItem(4);
+myTree.addItem(7);
+myTree.addItem(6);
+myTree.addItem(8);
+myTree.addItem(4);
+myTree.addItem(3);
+myTree.addItem(2);
+console.log(myTree);
+console.log('out all item tree -> depth-first search (DFS)');
 myTree.traverseDFS((node) => { console.log(node.value); }, 'preOrder');
-console.log('--search--');
-myTree.searchNode(8);
-/*
-console.log('--DFS pre--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'preOrder'
-);
-console.log('--DFS in--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'inOrder'
-);
-console.log('--DFS post--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'postOrder'
-);
-
-console.log('--BFS--');
-myTree.traverseBFS( (node: MyNode) => { console.log(node.value) } );
-*/ 
+console.log('hight tree:', myTree.hight);
+console.log('search item 7:', myTree.searchItem(7));
+console.log('delete item 7');
+myTree.deleteItem(7);
+console.log('search item 7:', myTree.searchItem(7));
+console.log('hight tree:', myTree.hight);
+console.log(myTree);
+myTree.traverseDFS((node) => { console.log(node.value); }, 'preOrder');
--- ts-tree-home-work/src/btree.ts
+++ ts-tree-home-work/src/btree.ts
@@ -23,6 +23,12 @@ class MyNode {
     set right(val: MyNode) {
         this._right = val;
     }
+
+    get height(): number {
+        let leftHeight = this.left ? this.left.height + 1 : 0;
+        let rightHeight = this.right ? this.right.height + 1 : 0;
+        return Math.max(leftHeight, rightHeight);
+    }
     get value(): number {
         return this._value;
     }
@@ -41,34 +47,32 @@ class MyBinaryTree {
         this._root = null;
     }
 
-    add(value: number): MyNode | null {
-        const newNode = new MyNode(value);
-        if (!this._root) {
-            this._root = newNode;
-            return newNode;
+    addItem(newItem: number): MyNode {
+        const newNode = new MyNode(newItem);
+        if (this._root === null) {
+            return this._root = newNode;
         }
-        let curNode = this._root;
-        while (curNode) {
-            if (newNode.value < curNode.value) {
-                if (!curNode.left) {
-                    curNode.left = newNode;
-                    return newNode;
-                }
-                else {
-                    curNode = curNode.left;
-                }
+        return this._addNode(this._root, newNode);
+    }
+    private _addNode(rootNode: MyNode, newNode: MyNode): MyNode {
+        let retVal: MyNode;
+        if (newNode.value < rootNode.value) {
+            if (rootNode.left === null) {
+                retVal = rootNode.left = newNode;
             }
             else {
-                if (!curNode.right) {
-                    curNode.right = newNode;
-                    return newNode;
-                }
-                else {
-                    curNode = curNode.right;
-                }
+                retVal = this._addNode(rootNode.left, newNode);
             }
         }
-        return newNode;
+        else {
+            if (rootNode.right === null) {
+                retVal = rootNode.right = newNode;
+            }
+            else {
+                retVal = this._addNode(rootNode.right, newNode);
+            }
+        }
+        return retVal;
     }
 
     preOrder(node: MyNode | null, callback: any) {
@@ -111,73 +115,139 @@ class MyBinaryTree {
         return this.postOrder(this._root, callback);
     }
 
-    recursionSearch(node: MyNode | null, value: number): MyNode | null {
-        if (!node) {
-            return null;
-        }
-        if (node.value === value) {
-            return node;
-            //console.log('!!!', node.value);
-        }
-        //console.log(node.value);
-        if (value > node.value) {
-            return this.recursionSearch(node.right, value);
-        }
-        else {
-            return this.recursionSearch(node.left, value);
-        }
+    searchItem(itemValue: number): MyNode | null {
+        return this._searchNode(this._root, itemValue);
     }
 
-    searchNode(value: number): MyNode | null {
-        return this.recursionSearch(this._root, value);
+    private _searchNode(node: MyNode | null, itemValue: number): MyNode | null {
+        if (node === null) {
+            return null;
+        }
+        if (node.value === itemValue) {
+            return node;
+        }
+        if (itemValue < node.value) {
+            return this._searchNode(node.left, itemValue);
+        }
+        else {
+            return this._searchNode(node.right, itemValue);
+        }
     }
     changeNode(value: number, newValue: number): MyNode | null {
-        const found = this.searchNode(value);
+        const found = this.searchItem(value);
         if (found) {
             found.value = newValue;
         }
         return found;
     }
+
+    deleteItem(itemValue: number): MyNode | null {
+        if (this._root === null) {
+            return null;
+        }
+        return this._root = this._deleteNode(this._root, itemValue);
+    }
+
+    private _deleteNode(currentNode: MyNode, itemValue: number): any {
+        // если нашли нужный элемент, начинаем процедуру удаления
+        if (currentNode.value === itemValue) {
+            // обработка самого простого случая, вместо узла возвращается null
+            if (currentNode.left === null && currentNode.right === null) {
+                return null;
+            }
+
+            // обработка двух случаев, с только одним из поддеревьев
+            if (currentNode.left === null) {
+                return currentNode.right;
+            }
+
+            if (currentNode.right === null) {
+                return currentNode.left;
+            }
+
+            // если у ноды есть оба потомка
+            const minNodeInRightSubtree = this._findMinElementRight(currentNode.right);
+            // заменили текущий элемент минимальным из правого поддерева
+            currentNode.value = minNodeInRightSubtree.value;
+
+            // ищем в правом поддереве минимальный элемент,
+            // значение которого уже вставлено на место текущего
+            currentNode.right = this._deleteNode(currentNode.right, minNodeInRightSubtree.value);
+            return currentNode;
+        }
+
+        // попадаем сюда, если элемент не был найден,
+        // просто проваливаемся в дерево глубже и глубже
+
+        // производится рекурсивный вызов этой же функции,
+        // при этом если элемент не будет найден,
+        // то алгоритм просто будет возвращать существующую ссылку на поддерево,
+        // которая присвоится в ту же позицию
+        if (itemValue < currentNode.value) {
+            if (currentNode.left === null) {
+                console.warn('Item not found!');
+                return currentNode;
+            }
+
+            // проваливаемся в левое поддерево,
+            // после рекурсивной отработки функции _deleteNode
+            // будет возвращен текущий элемент,
+            // который в предыдущем вызове будет присвоен
+            currentNode.left = this._deleteNode(currentNode.left, itemValue);
+
+            // присваивание на рекурсивный уровень выше,
+            // может быть как в левое поддерево,так и в правое,
+            // на текущем уровне мы не знаем какое поддерево обрабатываем
+            return currentNode;
+        }
+
+        // аналогичная обработка для правого поддерева
+        if (itemValue > currentNode.value) {
+            if (currentNode.right === null) {
+                console.warn('Item not found!');
+                return currentNode;
+            }
+
+            currentNode.right = this._deleteNode(currentNode.right, itemValue);
+            return currentNode;
+        }
+    }
+
+    private _findMinElementRight(node: MyNode): MyNode {
+        if (node.left === null) {
+            return node;
+        }
+        return this._findMinElementRight(node.left);
+    }
+    get hight (): number {
+        if (this._root === null) {
+            return 0;
+        }
+        return this._root.height;
+    }
 }
 
 const myTree = new MyBinaryTree();
-myTree.add(5);
-myTree.add(4);
-myTree.add(7);
-myTree.add(3);
-myTree.add(2);
-myTree.add(6);
-myTree.add(8);
-//console.log(myTree);
-
-console.log('--DFS pre--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'preOrder'
-);
-
-console.log('--search--');
-console.log(myTree.searchNode(4));
-
-console.log('--before change--');
+//--- insert item
+myTree.addItem(5);
+myTree.addItem(4);
+myTree.addItem(7);
+myTree.addItem(6);
+myTree.addItem(8);
+myTree.addItem(4);
+myTree.addItem(3);
+myTree.addItem(2);
 console.log(myTree);
-myTree.changeNode(4, 10);
-console.log('--after change--');
+console.log('out all item tree -> depth-first search (DFS)');
+myTree.traverseDFS( (node: MyNode) => { console.log(node.value); }, 'preOrder');
+
+console.log('hight tree:', myTree.hight);
+
+console.log('search item 7:', myTree.searchItem(7));
+
+console.log('delete item 7');
+myTree.deleteItem(7);
+console.log('search item 7:', myTree.searchItem(7));
+console.log('hight tree:', myTree.hight);
 console.log(myTree);
-
-/*
-console.log('--DFS pre--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'preOrder'
-);
-console.log('--DFS in--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'inOrder'
-);
-console.log('--DFS post--');
-myTree.traverseDFS((node: MyNode) => { console.log(node.value) },
-    'postOrder'
-);
-
-console.log('--BFS--');
-myTree.traverseBFS( (node: MyNode) => { console.log(node.value) } );
-*/
+myTree.traverseDFS( (node: MyNode) => { console.log(node.value); }, 'preOrder');
